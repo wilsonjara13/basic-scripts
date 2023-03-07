e7dea969cdfd1f8519292939dedbfcc193b4079f
wilsonjara13 <102395735+wilsonjara13@users.noreply.github.com>
2023-03-07 17:19:35 +0100

    Update hostdiscovery.sh

--- hostdiscovery.sh
+++ hostdiscovery.sh
@@ -3,8 +3,8 @@
 hosts=("192.168.1" "192.168.2")
 
 for host in ${hosts[@]};do
-        echo -e "\n[!] Enumerating $host.0"
+        echo -e "\n[!] Enumerating $host.0\n"
         for i in $(seq 1 254); do
-                timeout 1 bash -c "ping -c 1 $host.$i" &> /dev/null && echo "[+] Host $host.$i - ACTIVO" &
+                timeout 1 bash -c "ping -c 1 $host.$i" &> /dev/null && echo -e "\t[+] Host $host.$i - ACTIVO" &
         done; wait
 done
